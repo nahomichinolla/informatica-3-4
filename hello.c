36f89ff7b2bb9c3221304a691bb78d061bb51c76
nahomichinolla <chinollacamposnahomi@gmail.com>
2026-04-24 14:56:08 +0000

    hello updated

--- hello.c
+++ hello.c
@@ -1,6 +1,15 @@
+// Strings
 #include <stdio.h>
 
-int main(void)
-{
-printf("Hello  Nahomi! \n");
+int main(void){
+    char YourName[50];
+    printf("Your Name is: ");
+    scanf("%s", &YourName);
+    printf("Hello %s \n", YourName);
+    printf("\n");
+    char FavColor[50];
+    printf("Your fav color is: ");
+    scanf("%s", &FavColor);
+    printf("Fav Color: %s \n", FavColor);
+
 }

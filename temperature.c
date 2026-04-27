ad73ad76186e2fce0fda9a9bb80c606098cf7512
nahomichinolla <chinollacamposnahomi@gmail.com>
2026-04-27 17:22:40 +0000

    temperature

--- temperature.c
+++ temperature.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-int main(void){
-    
+
+int main(void)
+{
+    float Far;
+    printf("What is the temperature in farenheit: \n");
+    scanf("%f", &Far);
+    float Cels = (Far-32)/1.8;
+    printf("The temperature is %.2f F° = %2.f C° \n", Far, Cels); //The answer is -40 degrees
 }
+
+
 

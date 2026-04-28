01ad60f31222f30652c486d34473f4cb9af12bd1
nahomichinolla <chinollacamposnahomi@gmail.com>
2026-04-28 21:58:17 +0000

    minutes

---
+++ minutes.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+int main(void){
+    int mvt;
+    int min;
+    int hr;
+    printf("What is your starting time \n");
+    scanf("%d:%d", &hr, &min);
+    printf("How long is the movie \n")
+    scanf(%d", &mvt); 
+}

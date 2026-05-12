4cd4b933ce76ef66a0b19e2c212688dee5835d1b
nahomichinolla <chinollacamposnahomi@gmail.com>
2026-05-12 21:57:44 +0000

    calculator

---
+++ calc.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+//prtotype
+void add(void);
+void mult(void);
+void div(void);
+void sub(void);
+
+float n1;
+float n2;
+
+int main(void)
+{
+char ch;
+    printf("Equation: \n");
+    scanf("%f%c%f", &n1, &ch, &n2);
+    if (ch == )
+        add();
+}
+    else if (ch == )

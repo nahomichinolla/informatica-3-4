cbf8e353dc95d1169839f0153e2dde8d8bddddb8
nahomichinolla <chinollacamposnahomi@gmail.com>
2026-05-13 04:08:43 +0000

    calculator

--- calc.c
+++ calc.c
@@ -13,7 +13,50 @@ int main(void)
 char ch;
     printf("Equation: \n");
     scanf("%f%c%f", &n1, &ch, &n2);
-    if (ch == )
+    if (ch == '+'){
         add();
+    }
+    else if (ch == '-'){
+        sub();
+    }
+    else if (ch == '*'){
+        mult();
+    }
+    else if (ch == '/'){
+        div();
+    }
+ }
+
+void add(void){
+    //float n1;
+    //float n2;
+    // printf("Write your equation: \n");
+    // scanf("%f+%f",&n1, &n2);
+    float answer = n1+n2;
+    printf("%.2f\n", answer);
+ }
+void sub(void){
+    //float n1;
+    //float n2;
+    // printf("Write your equation: \n");
+    // scanf("%f-%f",&n1, &n2);
+    float answer = n1-n2;
+    printf("%.2f\n", answer);
+ }
+void mult(void){
+     //float n1;
+    //float n2;
+    // printf("Write your equation: \n");
+    // scanf("%f*%f",&n1, &n2);
+    float answer = n1*n2;
+    printf("%.2f\n", answer);
 }
-    else if (ch == )
+void div(void){
+
+     //float n1;
+    //float n2;
+    // printf("Write your equation: \n");
+    // scanf("%f/%f",&n1, &n2);
+    float answer = n1/n2;
+    printf("%.2f\n", answer);
+}
